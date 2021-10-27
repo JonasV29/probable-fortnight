b969591ebd1f576f8844e6d4de92804ebe7a54ba
Jonas Vitorio <jonasvitorio35@gmail.com>
2021-10-27 10:02:01 -0300

    adding a file

---
+++ Python/main.py
@@ -0,0 +1,27 @@
+#fazendo código em python
+import math #importando uma biblioteca matemática
+
+def conta(a, b):   #estou declarando uma função para se fazer uma conta matemática, de adição , multiplicação
+     s= a + b        # e de divisão e subtração
+     print(s)
+
+
+def multi(c, d):     #então assim que declara a função, no final eu escolho as variáveis e da o resultado final
+    y = c* d
+    print(y)
+
+
+def div(m, n):
+    z= m/ n
+    print(z)
+
+
+def dim(f, g):
+    x= f-g
+    print(x)
+
+
+conta(2, 8)
+multi(5, 7)
+div(10, 5)
+dim(34, 30) 
