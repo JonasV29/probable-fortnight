4a5b8cc1b39eb85c2296db893b08498c52b147bc
Jonas Vitorio <jonasvitorio35@gmail.com>
2023-02-21 00:55:24 -0300

    input in python

---
+++ login.py
@@ -0,0 +1,14 @@
+
+
+#This code , I learn about input in python
+# Here, I definate the variables 
+x = input(" Login:")
+y = input(" Password: ")
+music = input(' What is your favorite style music:')
+
+# Now, I put print , that in the end show what is  answer
+print('Hi,' , x , "with ", y, "password")
+print('Welcome!')
+print( "Congratulation, this", music, "is really music , Awesome")
+print('Great')
+
