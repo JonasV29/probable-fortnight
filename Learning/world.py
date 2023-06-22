37971bbc24aeeb2fb6b6b74aa732ff2d43d3c1a4
Jonas Vitorio <jonasvitorio35@gmail.com>
2023-06-22 19:38:29 -0300

    create a folder for my three codes

---
+++ Learning/world.py
@@ -0,0 +1,14 @@
+#learning python , how to print 
+#print("Hello World!")
+
+bill = 40
+
+if (bill < 25):
+    print("Bill is smaller")  # this is condition
+elif (bill > 25):
+     print("Bill is bigger")    #I use If...else 
+
+
+# It is interesting, because in Python use elif 
+# and not else
+
