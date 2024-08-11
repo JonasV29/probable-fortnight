fa83ad7081a1a44bd8d269c9229a5755255cf809
Jonas Vitorio <jonasvitorio35@gmail.com>
2024-08-10 22:24:49 -0300

    class in python

---
+++ class.py
@@ -0,0 +1,16 @@
+
+class automovel:
+    def __init__(self, roda, marca):
+      self.roda = roda
+      self.marca = marca
+
+class carro(automovel):
+    def __init__(self,roda,marca,combustivel):
+      self.combustivel = combustivel
+      super().__init__(roda,marca)
+
+
+car1 = carro(4, "chevrolet", "gasolina")
+print("Seu carro é da marca: ",car1.marca)
+print(f"Seu carro tem {car1.roda} rodas")
+print("Seu carro usa o combustivel: ",car1.combustivel)
